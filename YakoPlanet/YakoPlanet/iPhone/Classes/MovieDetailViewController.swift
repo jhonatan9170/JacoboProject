dbf309e7a1f3477cc87c10fb50355ea6967f7e5a
Jacobosrm <jacobosramirezm@gmail.com>
2023-04-24 19:39:44 -0500

    reto 02
    carga de datos desde un array

---
+++ YakoPlanet/YakoPlanet/iPhone/Classes/MovieDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MovieDetailViewController.swift
+//  YakoPlanet
+//
+//  Created by Jacobo Ramirez on 17/04/23.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+
+    
+    @IBOutlet weak var imgMovie:UIImageView!
+    @IBOutlet weak var lblTitle:UILabel!
+    @IBOutlet weak var lblReleaseDate:UILabel!
+    @IBOutlet weak var lblSummary:UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the viewj.
+        
+        
+        
+    }
+    
+
+    
+    
+}
---
+++ YakoPlanet/YakoPlanet/iPhone/Classes/MovieTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieTableViewCell.swift
+//  YakoPlanet
+//
+//  Created by Jacobo Ramirez on 22/04/23.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imgMovie:UIImageView!
+    @IBOutlet weak var lblTitle:UILabel!
+    @IBOutlet weak var lblReleaseDate:UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
